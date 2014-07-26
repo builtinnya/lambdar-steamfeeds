5e48d914271b0ebd096ca282c20c41dc3fe1bba9
Naoto Yokoyama <builtinnya@gmail.com>
2014-07-26 13:21:15 +0900

    Add license notice

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
+; (C) Copyright 2014 Naoto Yokoyama.
+;
+; All rights reserved. This program and the accompanying materials
+; are made available under the terms of the Eclipse Public License v1.0
+; which accompanies this distribution, and is available at
+; http://www.eclipse.org/legal/epl-v10.html
+;
+
 (defproject lambdar-steamfeeds "0.1.0-SNAPSHOT"
   :description "A feed generation service for Steam search results."
   :url "http://lambdar.info/steamfeeds/api"
